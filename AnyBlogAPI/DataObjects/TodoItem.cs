01a63c3e70a6be1e559bcddaadf57e33d24c6694
Greg Rairdon <grairdon88@gmail.com>
2015-07-21 20:53:35 -0400

    Initial Checking of AnyBlog API using Azure Mobile Services

---
+++ AnyBlogAPI/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace AnyBlogAPI.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
---
+++ AnyBlogAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnyBlogAPI.WebApiApplication" Language="C#" %>
